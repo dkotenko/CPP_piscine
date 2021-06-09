c9a8e49637ce4294b005cbee8d815ee147917e0b
Your Name <you@example.com>
2021-06-09 17:46:49 +0300

    makefile

---
+++ d01/cmake-build-debug/CMakeFiles/d01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/d01.dir/main.cpp.o"
+  "d01"
+  "d01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/d01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ d01/cmake-build-debug/CMakeFiles/d01.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for d01.
+# This may be replaced when dependencies are built.
