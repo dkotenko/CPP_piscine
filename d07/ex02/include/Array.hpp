922515fde101a19c88bb84f436f6c1bd2363ac1a
Cooper Lala <clala@oa-h5.msk.21-school.ru>
2023-02-23 11:25:49 +0300

    ex02 complete

--- d07/ex02/include/Array.hpp
+++ d07/ex02/include/Array.hpp
@@ -11,31 +11,60 @@ class Array
 	public:
 		Array() :
 			m_size(0),
-			m_array(nullptr);
+			m_array(new T[0]) {};
 		Array(unsigned int size) :
 			m_size(size),
 			m_array(new T[size]) {
-				for (unsigned int i = 0; i < size; i++) {
+				for (unsigned int i = 0; i < m_size; i++) {
 					m_array[i] = 0;
 				}
 		};
 		Array( Array const & src ) :
 			m_size(src.m_size),
 			m_array(new T[src.m_size]) {
-				for (unsigned int i = 0; i < size; i++) {
+				for (unsigned int i = 0; i < m_size; i++) {
 					m_array[i] = src.m_array[i];
 				}	
+		};
+		~Array() {
+			if (m_array != NULL) {
+				delete m_array;
 			}
-		~Array();
+		};
 
-		Array &		operator=( Array const & rhs );
+		Array &		operator=( Array const & rhs ) {
+			if (rhs.m_size != this->m_size) {
+				delete m_array;
+				m_size = rhs.m_size;
+				m_array = new T[m_size];
+			}
+			for (unsigned int i = 0; i < m_size; i++) {
+					m_array[i] = rhs.m_array[i];
+			}
+		};
+
+		T& operator[](unsigned int i) {
+			if (i > m_size - 1) {
+				throw std::runtime_error("Exception: index out of bounds");
+			}
+			return m_array[i];
+		};
+
+		unsigned int size() {
+			return m_size;
+		};
+
+		void print() {
+			for (unsigned int i = 0; i < m_size; i++) {
+					std::cout << m_array[i] << " ";
+			}
+			std::cout << std::endl;
+		}
 
 	private:
 		unsigned int m_size;
 		T *m_array;
 
 };
-
-std::ostream &			operator<<( std::ostream & o, Array const & i );
 
 #endif /* *********************************************************** ARRAY_H */
