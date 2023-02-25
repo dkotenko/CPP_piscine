a53434bd86093f8f604a7ed28caab23f4ffadb8b
Cooper Lala <clala@il-g6.msk.21-school.ru>
2023-02-25 14:06:34 +0300

    thanks to github.com/D-Daria for d08 that I totally copied

--- d08/ex02/include/MutantStack.hpp
+++ d08/ex02/include/MutantStack.hpp
@@ -23,9 +23,18 @@ class MutantStack : public std::stack<T> {
 		~MutantStack(){};
 
 		typedef typename MStack::container_type::iterator iterator;
+		typedef typename MStack::container_type::reverse_iterator reverse_iterator;
+		typedef typename MStack::container_type::const_iterator const_iterator;
+		typedef typename MStack::container_type::const_reverse_iterator const_reverse_iterator;
 
 		iterator begin();
 		iterator end();
+		reverse_iterator rbegin();
+		reverse_iterator rend();
+		const_iterator cbegin() const;
+		const_iterator cend() const;
+		const_reverse_iterator crbegin() const;
+		const_reverse_iterator crend() const;
 
 	private:
 };
@@ -38,4 +47,34 @@ typename MutantStack<T>::iterator MutantStack<T>::begin() {
 template<class T>
 typename MutantStack<T>::iterator MutantStack<T>::end() {
 	return (MStack::c.end());
+}
+
+template<class T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rbegin() {
+	return (MStack::c.rbegin());
+}
+
+template<class T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rend() {
+	return (MStack::c.rend());
+}
+
+template<class T>
+typename MutantStack<T>::const_iterator MutantStack<T>::cbegin() const {
+	return (MStack::c.cbegin());
+}
+
+template<class T>
+typename MutantStack<T>::const_iterator MutantStack<T>::cend() const {
+	return (MStack::c.cend());
+}
+
+template<class T>
+typename MutantStack<T>::const_reverse_iterator MutantStack<T>::crbegin() const {
+	return (MStack::c.crbegin());
+}
+
+template<class T>
+typename MutantStack<T>::const_reverse_iterator MutantStack<T>::crend() const {
+	return (MStack::c.crend());
 }
--- d08/ex02/src/main.cpp
+++ d08/ex02/src/main.cpp
@@ -3,7 +3,16 @@
 #include <list>
 #include <iostream>
 
-void subjectTest()
+void test(std::string s, void (*test)(void)) {
+    std::cout << "==============================\n";
+    std::cout << s << std::endl;
+    std::cout << "==============================\n";
+	test();
+	std::cout << "All testcases passed successfully\n";
+	std::cout << "******************************\n";
+}
+
+void test_subjectTest()
 {
 	MutantStack<int> mstack;
 	mstack.push(5);
@@ -34,7 +43,7 @@ void subjectTest()
 	std::stack<int> s(mstack);
 }
 
-void compare(void)
+void test_compare(void)
 {
 	std::list<int> list;
 
@@ -64,96 +73,106 @@ void compare(void)
 	}
 }
 
+void test_reverseIter() {
+	MutantStack<std::string> mstack;
+		
+	std::cout << "is empty " << mstack.empty() << std::endl;
+	std::string str[] = {"zero", "one", "two", "three", "four", "five", "six", "seven"};
+	for (size_t i = 0; i < sizeof(str) / sizeof(std::string); i++)
+		mstack.push(str[i]);
+	std::cout << "is empty " << mstack.empty() << std::endl;
+	MutantStack<std::string>::reverse_iterator rit;
+	for (rit = mstack.rbegin(); rit != mstack.rend(); rit++)
+		std::cout << *rit << std::endl;
+	std::cout << std::endl;
+
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.top() << std::endl;
+
+	rit--;
+	std::cout << *rit << std::endl;
+}
+
+void test_constReverseIter() {
+	MutantStack<float> mstack;
+	for (int i = 0; i < 10; i++) {
+		mstack.push((static_cast<float>(rand() % 100 + 1) / 2.1f));
+	}
+	mstack.push(42.4242);
+	mstack.push(21.2121);
+	std::cout << "const_iterator" << std::endl;
+	MutantStack<float>::const_iterator cit = mstack.cend() - 2;
+	std::cout << *cit << std::endl;
+	cit++;
+	std::cout << *cit << std::endl;
+	std::cout << std::endl;
+	for (cit = mstack.cbegin(); cit != mstack.cend(); cit++) {
+		std::cout << *cit << std::endl;
+	}
+	std::cout << std::endl;
+
+	std::cout << "const_reverse_iterator" << std::endl;
+	const MutantStack<float> const_mstack(mstack);
+	MutantStack<float>::const_reverse_iterator crit;
+	for (crit = const_mstack.crbegin(); crit != const_mstack.crend(); crit++)
+		std::cout << *crit << std::endl;
+}
+
+void test_all() {
+	MutantStack<char> mstack;
+	std::deque<char> deque;
+	for (int i = 0; i < 10; i++)
+	{
+		char ch = 'a' + rand() % 26;
+		mstack.push(ch);
+		deque.push_front(ch);
+	}
+	MutantStack<char>::iterator mit;
+	for (mit = mstack.begin(); mit != mstack.end(); mit++)
+		std::cout << "[" << *mit << "] ";
+	std::cout << std::endl;
+	std::deque<char>::iterator dit;
+	for (dit = mstack.begin(); dit != mstack.end(); dit++)
+		std::cout << "[" << *dit << "] ";
+	std::cout << std::endl;
+
+	std::cout << "temp:";
+	MutantStack<char> temp;
+	temp.push('a' + rand() % 26);
+	temp.push('a' + rand() % 26);
+	temp.push('a' + rand() % 26);
+	MutantStack<char>::iterator it;
+	for (it = temp.begin(); it != temp.end(); it++) {
+		std::cout << "[" << *it << "] ";
+	}
+	std::cout << '\n' << std::endl;
+
+	std::cout << "mstack.size: " << mstack.size() << std::endl;
+	std::cout << "temp.size: " << temp.size() << std::endl;
+
+	std::cout << "\ntemp.swap(mstack)" << std::endl;
+	temp.swap(mstack);
+	std::cout << "mstack.size: " << mstack.size() << std::endl;
+	for (mit = mstack.begin(); mit != mstack.end(); mit++) {
+		std::cout << "[" << *mit << "] ";
+	}
+		
+	std::cout << '\n' << std::endl;
+	std::cout << "temp.size: " << temp.size() << std::endl;
+	for (it = temp.begin(); it != temp.end(); it++) {
+		std::cout << "[" << *it << "] ";
+	}
+	std::cout << std::endl;
+}
+
 int main(void)
 {
-	srand(time(NULL));
-	std::cout << "-----------------" << std::endl;
-	std::cout << "-----Subject-----" << std::endl;
-	std::cout << "-----------------" << std::endl;
-	subjectTest();
-	std::cout << std::endl;
-	
-	std::cout << "-----------------" << std::endl;
-	std::cout << "-----Compare-----" << std::endl;
-	std::cout << "-----------------" << std::endl;
-	compare();
-	std::cout << std::endl;
-
-	{
-		std::cout << "----------------" << std::endl;
-		std::cout << "-----String-----" << std::endl;
-		std::cout << "----------------" << std::endl;
-		MutantStack<std::string> mstack;
-		
-		std::cout << "is empty " << mstack.empty() << std::endl;
-		std::string str[] = {"zero", "one", "two", "three", "four", "five", "six", "seven"};
-		for (size_t i = 0; i < sizeof(str) / sizeof(std::string); i++)
-			mstack.push(str[i]);
-		std::cout << "is empty " << mstack.empty() << std::endl;
-		std::cout << std::endl;
-
-		std::cout << mstack.top() << std::endl;
-		mstack.pop();
-		std::cout << mstack.top() << std::endl;
-		mstack.pop();
-		std::cout << mstack.top() << std::endl;
-	}
-	std::cout << std::endl;
-	{
-		std::cout << "---------------" << std::endl;
-		std::cout << "-----float-----" << std::endl;
-		std::cout << "---------------" << std::endl;
-		MutantStack<float> mstack;
-
-		for (int i = 0; i < 10; i++)
-			mstack.push((static_cast<float>(rand() % 100 + 1) / 2.1f));
-		mstack.push(42.4242);
-		mstack.push(21.2121);
-	}
-	std::cout << std::endl;
-	{
-		std::cout << "--------------" << std::endl;
-		std::cout << "-----char-----" << std::endl;
-		std::cout << "--------------" << std::endl;
-		MutantStack<char> mstack;
-		std::deque<char> deque;
-		for (int i = 0; i < 10; i++)
-		{
-			char ch = 'a' + rand() % 26;
-			mstack.push(ch);
-			deque.push_front(ch);
-		}
-		MutantStack<char>::iterator mit;
-		for (mit = mstack.begin(); mit != mstack.end(); mit++)
-			std::cout << "[" << *mit << "] ";
-		std::cout << std::endl;
-		std::deque<char>::iterator dit;
-		for (dit = mstack.begin(); dit != mstack.end(); dit++)
-			std::cout << "[" << *dit << "] ";
-		std::cout << std::endl;
-
-		std::cout << "temp:";
-		MutantStack<char> temp;
-		temp.push('a' + rand() % 26);
-		temp.push('a' + rand() % 26);
-		temp.push('a' + rand() % 26);
-		MutantStack<char>::iterator it;
-		for (it = temp.begin(); it != temp.end(); it++)
-			std::cout << "[" << *it << "] ";
-		std::cout << '\n' << std::endl;
-
-		std::cout << "mstack.size: " << mstack.size() << std::endl;
-		std::cout << "temp.size: " << temp.size() << std::endl;
-
-		std::cout << "\ntemp.swap(mstack)" << std::endl;
-		temp.swap(mstack);
-		std::cout << "mstack.size: " << mstack.size() << std::endl;
-		for (mit = mstack.begin(); mit != mstack.end(); mit++)
-			std::cout << "[" << *mit << "] ";
-		std::cout << '\n' << std::endl;
-		std::cout << "temp.size: " << temp.size() << std::endl;
-		for (it = temp.begin(); it != temp.end(); it++)
-			std::cout << "[" << *it << "] ";
-		std::cout << std::endl;
-	}
+	test("subjectTest", &test_subjectTest);
+	test("compare", &test_compare);
+	test("reverseIter", &test_reverseIter);
+	test("constReverseIter", &test_constReverseIter);
+	test("all", &test_all);
 }
